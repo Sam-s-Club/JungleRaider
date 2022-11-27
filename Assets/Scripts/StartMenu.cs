e0b8545aecf6b3ea5b315891cddddd85775211ac
zaskins <zaskins8209@sdsu.edu>
2022-11-27 14:45:53 -0800

    Adding the scripts that pertain to the menus

--- Assets/Scripts/StartMenu.cs
+++ Assets/Scripts/StartMenu.cs
@@ -8,23 +8,23 @@ using UnityEditor;
 
 public class StartMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
     }
 
+    //load the game upon clicking the start button
     public void StartNew()
     {
         SceneManager.LoadScene(1);
     }
 
+    //exit the game, checks if in the editor or in the executable
     public void Exit()
     {
         #if UNITY_EDITOR
