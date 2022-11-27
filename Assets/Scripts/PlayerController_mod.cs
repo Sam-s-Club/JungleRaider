de8e617ec49d7eee9fc670d5497b61a34a4d55a0
zaskins <zaskins8209@sdsu.edu>
2022-11-27 14:53:40 -0800

    Changes that are on development branch

---
+++ Assets/Scripts/PlayerController_mod.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController_mod : MonoBehaviour
+{
+    Rigidbody rb;
+    public float horizontalInput;
+    public float verticalInput;
+    public bool jumpInput;
+    public float speed = 10.0f;
+    public float jumpSpeed = 10.0f;
+
+    void Awake() {
+        rb = GetComponent<Rigidbody>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        
+        Debug.Log(Input.GetKeyDown(KeyCode.Space));
+   
+        // transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        // transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
+        if(Input.GetKeyDown(KeyCode.A)){
+            transform.Rotate(0,-90,0,Space.World);
+        }
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            Jump();
+        }
+    }
+    void Jump() {
+        rb.AddForce(Vector3.up * jumpSpeed, ForceMode.Impulse);
+    }
+    void FixedUpdate() {
+        //rigidbody.velocity = new Vector3()
+        //jumpInput = Input.GetKeyDown(KeyCode.Space);
+    }
+    
+}
---
+++ Assets/Scripts/Player_mod.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Player_mod : MonoBehaviour
+{
+    public GameObject gameMenu;
+    public TextMeshProUGUI text;
+
+    void Start() {
+        
+    }
+
+    //checking if the player has fallen off the world
+    void Update(){
+        if(transform.position.y < 0){
+            GameOver();
+        }
+    }
+
+    //checking collision tags and setting game menu depending on the tag
+    void OnCollisionEnter(Collision col){
+        if (col.gameObject.tag == "enemy") {
+            GameOver();
+        }
+        else if(col.gameObject.tag == "victory"){
+            Victory();
+        }
+    }
+
+    void Victory(){
+        text.text = "Victory";
+        gameMenu.gameObject.SetActive(true);
+    }
+
+    void GameOver(){
+        text.text = "Game Over";
+        gameMenu.gameObject.SetActive(true);
+
+    }
+}
