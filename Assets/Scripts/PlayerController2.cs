14a358af9d48fde3721149bab55e81fb8aa70916
agopez97 <104110693+agopez97@users.noreply.github.com>
2022-12-07 16:01:46 -0800

    Changed Character and Allowed interaction between character and stage

--- Assets/Scripts/PlayerController2.cs
+++ Assets/Scripts/PlayerController2.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class PlayerController2 : MonoBehaviour
 {
     private Rigidbody rg;
+    private Animator animator;
     public GameObject GroundCheck;
+    private Vector3 moveDirection;
+    public AudioSource runSound;
+    public AudioSource jumpSound;
     public float speed = 10.0F;
     public float jumpspeed = 10.0F;
     public float gravSpeed = 10.0F;
@@ -14,6 +18,7 @@ public class PlayerController2 : MonoBehaviour
    
     void Start () {
         rg = GetComponent <Rigidbody> ();
+        animator = GetComponent <Animator> ();
     }
    
    
@@ -22,17 +27,25 @@ public class PlayerController2 : MonoBehaviour
         float strafe = Input.GetAxis ("Horizontal") * speed;
         translation *= (Time.deltaTime);
         strafe *= (Time.deltaTime);
-       
+        moveDirection = new Vector3(translation, 0, strafe);
         transform.Translate (strafe, 0, translation);
         grounded = Physics.CheckSphere(GroundCheck.transform.position, .2f, LayerMask.GetMask("Ground"));
        
         if (Input.GetKey (KeyCode.Space) && grounded) {
             Jump();
         }
+        if (moveDirection == Vector3.zero) {
+            animator.SetFloat("Speed", 0);
+        }
+        else {
+            animator.SetFloat("Speed", 1);
+            runSound.Play();
+        }
        
     }
     void Jump(){
         rg.velocity = new Vector3(rg.velocity.x, jumpspeed, 0);
+        jumpSound.Play();
     }
     
     void FixedUpdate(){
