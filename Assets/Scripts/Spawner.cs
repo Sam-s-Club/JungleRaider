9e27fd743870affc486edef33c5602df21502fa7
zaskins <zaskins8209@sdsu.edu>
2022-12-15 14:23:38 -0800

    Almost done, just need to update texture and change some things

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -3,15 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
-{
-    public int difficulty;
-    
+{   
     // checking to see if this spawner will spawn an enemy and if so figure out which enemy will be spawned
     void Start()
     {
         //determine if an enemy should be spawned based on random number in range of difficulty
         //lower difficulty means higher number and greater range
-        if(Random.Range(1,3)==1){
+        if(Random.Range(1,1)==1){
             SpawnEnemy();
         }
     }
@@ -20,10 +18,10 @@ public class Spawner : MonoBehaviour
     {
         //determine enemy to be spawned by random number, will load the chosen enemy grabbing the prefab from the resources folder
             if(Random.Range(0,2)==0){
-                Instantiate(Resources.Load<GameObject>("Spider"),transform.position,Quaternion.Euler(180,0,0));
+                Instantiate(Resources.Load<GameObject>("Spider"),(transform.position + new Vector3(0,0.5f,0)),Quaternion.Euler(180,0,0));
             }
             else{
-                Instantiate(Resources.Load<GameObject>("Snakes"),transform.position,transform.rotation);
+                Instantiate(Resources.Load<GameObject>("bugspin"),(transform.position+ new Vector3(-2f,0.15f,0)),transform.rotation);
             }
     }
 }
