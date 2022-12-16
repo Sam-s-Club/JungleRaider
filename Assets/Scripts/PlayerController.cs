904a0381d0482bea65f128cad5d547810457c190
Matt <61751438+Mattz-CE@users.noreply.github.com>
2022-12-15 23:47:02 -0800

    Gave up on motion control; zelda

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,10 +10,17 @@ public class PlayerController : MonoBehaviour
     private Vector3 moveDirection;
     public AudioSource runSound;
     public AudioSource jumpSound;
-    public float turnspeed = 10.0f;
-    public float speed = 10.0F;
-    public float jumpspeed = 10.0F;
-    public float gravSpeed = 10.0F;
+
+    public float turnspeed = 15.0f;
+    public float movementSpeed = 7.0F;
+    public float jumpspeed = 14.0F;
+    public float gravSpeed = 15.0F;
+    public float terminalVelocity = 10.0F;
+    public float diminishingMultiplier = 0.75f;
+
+    public float jumpDelay = 0.25f;
+    public float jumpTolerance = 0.6f;
+
     bool grounded;
     
     //get the animator and the rigidbody
@@ -26,8 +33,8 @@ public class PlayerController : MonoBehaviour
     //checking keys, doing movement and animation
     void Update () 
     {
-        //animator and sound being set
-        if (moveDirection == Vector3.zero) 
+        // Set animation according to movement speed
+        if (moveDirection == Vector3.zero)
         {
             animator.SetBool("Run",false);
         }
@@ -36,20 +43,55 @@ public class PlayerController : MonoBehaviour
             runSound.Play();
             animator.SetBool("Run",true);
         }
-        
-        //movement handeling
-        float translation = Input.GetAxis ("Vertical") * speed;
-        float strafe = Input.GetAxis ("Horizontal") * speed;
-        translation *= (Time.deltaTime);
-        strafe *= (Time.deltaTime);
-        moveDirection = new Vector3(translation, 0, strafe);
-        transform.Translate (strafe, 0, translation, Camera.main.transform);
-        
-        grounded = Physics.CheckSphere(GroundCheck.transform.position, .2f, LayerMask.GetMask("Ground"));//checking if character will jump
-        if (Input.GetKey (KeyCode.Space) && grounded) 
+
+        // check terminal velocity
+        if (rg.velocity.y < -terminalVelocity)
         {
+            rg.velocity = new Vector3(rg.velocity.x, -terminalVelocity, 0);
+        }
+
+        // Diminishes the velocity if going up
+        if (rg.velocity.y > 0)
+        {
+            rg.velocity = new Vector3(rg.velocity.x, rg.velocity.y * diminishingMultiplier, 0);
+        }
+
+        // acclerates down if falling enables fast falling
+        if (rg.velocity.y < 0)
+        {
+            rg.velocity = new Vector3(rg.velocity.x, rg.velocity.y - (gravSpeed * Time.deltaTime), 0);
+        }
+        
+
+        //movement handeling
+        float translation = Input.GetAxis ("Vertical") * movementSpeed;
+        float strafe = Input.GetAxis ("Horizontal") * movementSpeed;
+
+        // manage speed
+        translation *= Time.deltaTime;
+        strafe *= Time.deltaTime;
+         
+        // move the character
+        transform.Translate (strafe, 0, translation, Camera.main.transform);
+        moveDirection = new Vector3(translation, 0, strafe);
+
+        // check if the player is on the ground        
+        grounded = Physics.CheckSphere(GroundCheck.transform.position, .2f, LayerMask.GetMask("Ground"));//checking if character will jump
+        
+        
+        if (grounded && Input.GetButtonDown("Jump")) 
+        {
+            // check how long the player pressed until jumpDelay and change the jump speed accordingly
+            float jumpTime = Mathf.Clamp01(Input.GetAxis("Jump") / jumpDelay);
             Jump();
         }
+
+    }
+
+    // check how close the player is to the ground
+    private bool CloseEnoughToGround()
+    {
+        return Physics.CheckSphere(GroundCheck.transform.position, jumpTolerance, LayerMask.GetMask("Ground"));
     }
 
     //jump function for character and handles sound
@@ -63,10 +105,11 @@ public class PlayerController : MonoBehaviour
     void FixedUpdate()
     {
         Vector3 dir = Vector3.zero;
- 
+        
+        // Mouse look
         dir.x = Input.GetAxis("Horizontal");
         dir.z = Input.GetAxis("Vertical");
-
+        
         Vector3 camDirection = Camera.main.transform.rotation * dir;
         Vector3 targetDirection = new Vector3(camDirection.x, 0, camDirection.z);//make the player look in the direction of the camera is facing
             
